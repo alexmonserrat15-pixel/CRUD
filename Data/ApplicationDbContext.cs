b1a4559fd3d8f60038111c12cea1a8e7694db977
Yohanser <20242537@itla.edu.do>
2026-07-29 18:07:43 -0400

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+
+
+    }
+}
